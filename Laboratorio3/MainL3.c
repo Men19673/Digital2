a2c44c38011dd431d718d9c9ceda5246cd5cd8b3
Men19673 <men19673@uvg.edu.gt>
2021-08-01 20:46:27 -0600

    Inicio Lab 3

---
+++ Laboratorio3/MainL3.c
@@ -0,0 +1,18 @@
+/* 
+ * File:   MainL3.c
+ * Author: diego
+ *
+ * Created on August 1, 2021, 8:40 PM
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return (EXIT_SUCCESS);
+}
+
